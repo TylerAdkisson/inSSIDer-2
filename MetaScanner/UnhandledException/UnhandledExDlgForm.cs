e1e9d71d34f7900d8f43214d7d5680414019ed4e
Tyler Adkisson <tyleradkisson@gmail.com>
2011-01-20 01:18:06 -0500

    Tyler: Updated and added license headers to code files

--- MetaScanner/UnhandledException/UnhandledExDlgForm.cs
+++ MetaScanner/UnhandledException/UnhandledExDlgForm.cs
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////
 //
-// Copyright (c) 2007-2010 MetaGeek, LLC
+// Copyright (c) 2007-2011 MetaGeek, LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); 
 // you may not use this file except in compliance with the License. 
